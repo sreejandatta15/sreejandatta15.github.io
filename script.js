fc97d8243b6baee1a2310ab628775d87785ff2d4
Sreejan Datta <146056134+sreejandatta15@users.noreply.github.com>
2024-05-22 13:06:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const heroContent = document.querySelector('.hero-content');
+    heroContent.style.opacity = '0';
+    heroContent.style.transform = 'translateY(-50px)';
+    setTimeout(() => {
+        heroContent.style.transition = 'opacity 1s ease-out, transform 1s ease-out';
+        heroContent.style.opacity = '1';
+        heroContent.style.transform = 'translateY(0)';
+    }, 500);
+});
